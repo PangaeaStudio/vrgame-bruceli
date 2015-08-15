04eb621efd21c4a23c86338bdf765642b5bd9f66
huodianyan <huodianyan@163.com>
2015-08-15 19:22:02 +0800

    zombie pack

--- Assets/ViveTest/Scrips/MonsterController.cs
+++ Assets/ViveTest/Scrips/MonsterController.cs
@@ -1,8 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using RootMotion.Demos;
 
 public class MonsterController : MonoBehaviour
 {
+    HitReaction hitReaction;
+    Rigidbody rigidbody;
+
+    void Awake()
+    {
+        hitReaction = GetComponent<HitReaction>();
+
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+	public void Hit(Collider collider, Vector3 direction, Vector3 point, int damage)
+    {
+        // Use the HitReaction
+        hitReaction.Hit(collider, direction, point);
+
+        direction.y = 0;
+        rigidbody.velocity = (direction.normalized + (Vector3.up * 1)) * 1 * 1;
+
+    }
+
+    public void Grab(Vector3 direction)
+    {
+    }
 
     // Use this for initialization
     void Start()
